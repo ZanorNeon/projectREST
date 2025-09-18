f5fece9b051fd7584f30e4bda690e86fff6a89de
Alex <owner@slaand.com>
2025-09-18 20:24:22 +0100

    flyway fixes

--- projectREST/src/main/resources/db/migration/V1__init.sql
+++ projectREST/src/main/resources/db/migration/V1__init.sql
@@ -7,7 +7,7 @@ CREATE TABLE users (
     id BIGSERIAL PRIMARY KEY,
     email VARCHAR(255) UNIQUE NOT NULL,
     password VARCHAR(255) NOT NULL,
-    fullname VARCHAR(255) NOT NULL,
+    full_name VARCHAR(255) NOT NULL,
     created_at TIMESTAMP DEFAULT now(),
     updated_at TIMESTAMP DEFAULT now()
 );
