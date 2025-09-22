748568ca41826fe511b45b60a685740ecbfa6023
ZanorNeon <raytherayoor@gmail.com>
2025-09-22 18:47:20 +0300

    500 server error my beloved

--- projectREST/src/main/resources/db/migration/V3__seed_admin_user.sql
+++ projectREST/src/main/resources/db/migration/V3__seed_admin_user.sql
@@ -1,6 +1,6 @@
 INSERT INTO users (email, password, full_name)
 VALUES ('admin@example.com',
-        '$2a$10$Dow1pQUnOtYt1wJv4FR8ZOe/UPuM9N4fYQZPfrxJ0cUO4nWx8mPLa',
+        '$2a$10$/6smY4RZ0orZiTM3iZFPOeoOdzkVqMbFNnS8a/6cU.OiUCckCCy9W',
         'Admin User');
 
 INSERT INTO user_roles (user_id, role_id)
